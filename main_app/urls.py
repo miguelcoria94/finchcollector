c3fb18920a61a718222fceab74368471bdcf9df1
miguelcoria94 <miguelcoria94@gmail.com>
2024-05-03 17:11:43 -0500

    flinch collector part 3 done

--- main_app/urls.py
+++ main_app/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
     path('flinch/', views.flinch_index, name='index'),
+    path('flinch/<int:flinch_id>/', views.flinch_detail, name='detail'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from .models import Flinch
 
 # Create your views here.
 
@@ -9,5 +10,9 @@ def about(request):
     return render(request, 'about.html' , {'author': 'Your Name'})
 
 def flinch_index(request):
-    return render(request, 'flinch/index.html'
-    , {'cats': ['Luna', 'Cricket', 'Pumpkin']})
+    finches = Flinch.objects.all()
+    return render(request, 'flinch/index.html', { 'flinches': finches})
+
+def flinch_detail(request, flinch_id):
+  flinch = Flinch.objects.get(id=flinch_id)
+  return render(request, 'flinch/detail.html', { 'flinch': flinch })
